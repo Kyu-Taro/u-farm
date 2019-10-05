085af146e9a7bc83649717263a2a96c29bb21132
Kyu Taro <kyutaro@Kyu.local>
2019-10-05 11:10:10 +0900

    commit

--- app/Http/Controllers/ChargeController.php
+++ app/Http/Controllers/ChargeController.php
@@ -13,6 +13,7 @@ use App\Mail\Admin;
 use App\Mail\User as Users;
 use App\Mail\Me;
 use App\Admin as Admins;
+use App\Account;
 
 class ChargeController extends Controller
 {
@@ -57,22 +58,22 @@ class ChargeController extends Controller
         $adminNumber = $item->admin->tell;
         Mail::to($to)->send(new Users($name,$price,$adminName,$adminNumber));
 
-        $to = 'amiti999@icloud.com';
-        $userName = $user->naem;
-        $userNumber = $user->tell;
-        $userPostNumber = $user->postNumber;
-        $userArea = $user->area;
-        $itemName = $item->name;
-        $itemPrice = $item->price;
-        $adminName = $item->admin->name;
-        $adminPostNumber = $item->admin->postNumber;
-        $adminArea = $item->admin->area;
-        $bankName = $admin->account->bank;
-        $bankAdminName = $admin->account->name;
-        $bankBranch = $admin->account->branch;
-        $bankType = $admin->account->type;
-        $bankNumber = $admin->account->bankNumber;
-        Mail::to($to)->send(new Me($userName,$userNumber,$userPostNumber,$userArea,$itemName,$itemPrice,$adminName,$adminPostNumber,$adminArea,$bankName,$bankAdminName,$bankBranch,$bankType,$bankNumber));
+        // $to = 'amiti999@icloud.com';
+        // $userName = $user->naem;
+        // $userNumber = $user->tell;
+        // $userPostNumber = $user->postNumber;
+        // $userArea = $user->area;
+        // $itemName = $item->name;
+        // $itemPrice = $item->price;
+        // $adminName = $item->admin->name;
+        // $adminPostNumber = $item->admin->postNumber;
+        // $adminArea = $item->admin->area;
+        // $bankName = $admin->account->bank;
+        // $bankAdminName = $admin->account->name;
+        // $bankBranch = $admin->account->branch;
+        // $bankType = $admin->account->type;
+        // $bankNumber = $admin->account->bankNumber;
+        // Mail::to($to)->send(new Me($userName,$userNumber,$userPostNumber,$userArea,$itemName,$itemPrice,$adminName,$adminPostNumber,$adminArea,$bankName,$bankAdminName,$bankBranch,$bankType,$bankNumber));
 
         return back();
     }
