5d3b8ec9bac9862b3d40327c35ac9dbf323e92a1
Kyu Taro <kyutaro@Kyu.local>
2019-10-06 16:46:01 +0900

    commit!

--- database/migrations/2019_10_06_073622_add_column_items_table.php
+++ database/migrations/2019_10_06_073622_add_column_items_table.php
@@ -14,7 +14,7 @@ class AddColumnItemsTable extends Migration
     public function up()
     {
         Schema::table('items', function (Blueprint $table) {
-            $table->integer('day');
+            $table->integer('day')->nullable();
         });
     }
 
