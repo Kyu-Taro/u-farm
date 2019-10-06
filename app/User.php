8f83191d5badd474f89e734a29cbc5238e0027cc
Kyu Taro <kyutaro@Kyu.local>
2019-10-06 18:02:32 +0900

    commit

--- app/User.php
+++ app/User.php
@@ -38,4 +38,9 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function sendEmailVerificationNotification()
+    {
+        $this->notify(new \App\Notifications\VerifyEmail\Japanese);
+    }
 }
