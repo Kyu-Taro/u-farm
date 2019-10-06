14546a6d658c069354c11f18251aa7cae3530062
Kyu Taro <kyutaro@Kyu.local>
2019-10-06 20:46:27 +0900

    commit

--- app/Notifications/VerifyEmailJapanese.php
+++ app/Notifications/VerifyEmailJapanese.php
@@ -8,7 +8,7 @@ use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\URL;
-use Illuminate\Support\facades\Lang;
+use Illuminate\Support\Facades\Lang;
 
 class VerifyEmailJapanese extends Notification
 {
