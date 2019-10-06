d2b446db1b0de04d8e521f4f2125a9d7407f846d
Kyu Taro <kyutaro@Kyu.local>
2019-10-06 17:58:21 +0900

    commit!

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -17,7 +17,7 @@
                     仮登録のメールアドレス宛に、メールを送信しました。そちらのリンクから本登録を行ってください
                     <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
                         @csrf
-		                メールが届かない場合は<button type="submit" class="btn btn-link p-0 m-0 align-baseline">こちらをクリック</button>.
+		                メールが届かない場合は<button type="submit" class="btn btn-link p-0 m-0 align-baseline">こちらをクリック</button>
 	                </form>
                 </div>
             </div>
