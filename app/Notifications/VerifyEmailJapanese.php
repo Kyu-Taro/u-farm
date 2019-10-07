3318aa5be8ad5b60fe02d8edf9992c2daefe37d0
Kyu Taro <kyutaro@Kyu.local>
2019-10-07 20:13:34 +0900

    commit

--- app/Notifications/VerifyEmailJapanese.php
+++ app/Notifications/VerifyEmailJapanese.php
@@ -1,14 +1,15 @@
 <?php
 
-namespace App\Notifications;
+namespace Illuminate\Auth\Notifications;
 
-use Illuminate\Support\Carbon;
-use Illuminate\Support\Facades\URL;
-use Illuminate\Support\Facades\Lang;
-use Illuminate\Notifications\Notification;
 use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Lang;
+use Illuminate\Support\Facades\URL;
 
-class VerifyEmailJapanese extends Notification
+class VerifyEmail extends Notification
 {
     /**
      * The callback that should be used to build the mail message.
@@ -36,18 +37,17 @@ class VerifyEmailJapanese extends Notification
      */
     public function toMail($notifiable)
     {
+        $verificationUrl = $this->verificationUrl($notifiable);
+
         if (static::$toMailCallback) {
-            return call_user_func(static::$toMailCallback, $notifiable);
+            return call_user_func(static::$toMailCallback, $notifiable, $verificationUrl);
         }
 
         return (new MailMessage)
-            ->subject(Lang::getFromJson('本登録メール'))
-            ->line(Lang::getFromJson('以下の認証リンクをクリックして本登録を完了させてください。'))
-            ->action(
-                Lang::getFromJson('メールアドレスを認証する'),
-                $this->verificationUrl($notifiable)
-            )
-            ->line(Lang::getFromJson('もしこのメールに覚えが無い場合は破棄してください。'));
+            ->subject(Lang::get('Verify Email Address'))
+            ->line(Lang::get('Please click the button below to verify your email address.'))
+            ->action(Lang::get('Verify Email Address'), $verificationUrl)
+            ->line(Lang::get('If you did not create an account, no further action is required.'));
     }
 
     /**
@@ -59,7 +59,12 @@ class VerifyEmailJapanese extends Notification
     protected function verificationUrl($notifiable)
     {
         return URL::temporarySignedRoute(
-            'verification.verify', Carbon::now()->addMinutes(60), ['id' => $notifiable->getKey()]
+            'verification.verify',
+            Carbon::now()->addMinutes(Config::get('auth.verification.expire', 60)),
+            [
+                'id' => $notifiable->getKey(),
+                'hash' => sha1($notifiable->getEmailForVerification()),
+            ]
         );
     }
 
