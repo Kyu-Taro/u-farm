d38e0d3947632596a1037ae535a70f0b5c02a44d
Kyu Taro <kyutaro@Kyu.local>
2019-10-10 22:17:29 +0900

    commit

--- resources/views/pages/itemDetail.blade.php
+++ resources/views/pages/itemDetail.blade.php
@@ -67,30 +67,37 @@
 @endsection
 
 @section('content')
-    <img src="@if(empty($item->img)) /img/sample.jpg @else {{ $item->img }} @endif"><br/>
-    生産者:<a href="/admin/{{ $item->admin->id }}">{{ $item->admin->name }}</a><br/>
-    商品名:{{ $item->name }}<br/>
-    料金:{{ $item->price }}円<br/>
-    産地:{{ $item->area }}<br/>
-    発送までの日数:{{ $item->day }}日<br/>
-    商品説明:<br/>
-    {{ $item->text }}<br/>
-    <form action="/charge/{{ $item->id }}/{{ $user->id }}" method="POST">
-        @csrf
-        @if (Auth::check())
-            <script
-                src="https://checkout.stripe.com/checkout.js" class="stripe-button"
-                data-key="{{ env('STRIPE_KEY') }}"
-                data-amount="{{ $item->price }}"
-                data-name="Stripe Demo"
-                data-label="決済をする"
-                data-description="Online course about integrating Stripe"
-                data-image="https://stripe.com/img/documentation/checkout/marketplace.png"
-                data-locale="auto"
-                data-currency="JPY">
-            </script>
-        @endif
-        </form>
+    <h1 class="detail-title">商品詳細</h1>
+    <div class="items-img">
+        <img src="@if(empty($item->img)) /img/sample.jpg @else {{ $item->img }} @endif"><br/>
+    </div>
+    <div class="item">
+        <p>生産者:<a href="/admin/{{ $item->admin->id }}">{{ $item->admin->name }}</a></p>
+        <p>商品名:{{ $item->name }}</p>
+        <p>料金:{{ $item->price }}円</p>
+        <p>産地:{{ $item->area }}</p>
+        <p>発送までの日数:{{ $item->day }}日</p>
+        商品説明:<br/>
+        <div class="item-text">
+        {{ $item->text }}
+        </div>
+        <form action="/charge/{{ $item->id }}/{{ $user->id }}" method="POST">
+            @csrf
+            @if (Auth::check())
+                <script
+                    src="https://checkout.stripe.com/checkout.js" class="stripe-button"
+                    data-key="{{ env('STRIPE_KEY') }}"
+                    data-amount="{{ $item->price }}"
+                    data-name="Stripe Demo"
+                    data-label="決済をする"
+                    data-description="Online course about integrating Stripe"
+                    data-image="https://stripe.com/img/documentation/checkout/marketplace.png"
+                    data-locale="auto"
+                    data-currency="JPY">
+                </script>
+            @endif
+            </form>
+        </div>
 @endsection
 
 @section('footer')
