6711276475035c14a1fc7e391643827cd01f4cf8
神谷友樹 <jetree900@gmail.com>
2019-11-01 23:29:42 +0900

    index.blade.php作成

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -12,7 +12,8 @@ class MainController extends Controller
 {
     public function index()
     {
-        return view('pages.index');
+        // return view('pages.index');
+        return view('index');
     }
 
     public function register_second()
---
+++ resources/views/index.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <link rel="stylesheet" href="/css/index.css" type="text/css">
+    <link rel="stylesheet" href="css/app.css" type="text/css">
+
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+    
+    <title>U-farm | トップページ</title>
+</head>
+<body>
+    <div id="app">
+        <example-component></example-component>
+    </div>
+    <script src='js/app.js'></script>
+</body>
