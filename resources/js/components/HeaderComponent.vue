38d43d7f19815944eb82bde9855b80a2e617b9e2
神谷友樹 <jetree900@gmail.com>
2019-11-04 22:35:15 +0900

    HeaderComponentのstyle修正

--- resources/js/components/HeaderComponent.vue
+++ resources/js/components/HeaderComponent.vue
@@ -15,49 +15,49 @@
 </template>
 
 <style scoped>
-#title{
-    margin-bottom: 0;
-    float: left;
-}
-#header-area{
-    max-width: 980px;
-    margin: 0 auto;
-}
-header{
-    height: 80px;
-    background: #703f27;
-    padding: 0;
-    margin: 0;
-    width: 100%;
-}
-h1{
-    margin-bottom: 0;
-}
+    #title{
+        margin-bottom: 0;
+        float: left;
+    }
+    #header-area{
+        max-width: 980px;
+        margin: 0 auto;
+    }
+    header{
+        height: 80px;
+        background: #703f27;
+        padding: 0;
+        margin: 0;
+        width: 100%;
+    }
+    h1{
+        margin-bottom: 0;
+    }
 
-nav{
-    float: right;
-    display: flex;
-    align-items: center;
-    height: 80px;
-}
-a:visited{
-    color:white
-}
-.nav-item{
-    margin: 0 10px;
-    font-size: 1.2rem;
-}
-.nav-item a:hover{
-    text-decoration: underline;
-}
-.nav-item a{
-    text-decoration: none;
-    color: white;
-}
-.header-title{
-    line-height: 80px;
-    padding-left: 50px;
-    font-size: 2.6rem;
-    color: white;
-}
+    nav{
+        float: right;
+        display: flex;
+        align-items: center;
+        height: 80px;
+    }
+    a:visited{
+        color:white
+    }
+    .nav-item{
+        margin: 0 10px;
+        font-size: 1.2rem;
+    }
+    .nav-item a:hover{
+        text-decoration: underline;
+    }
+    .nav-item a{
+        text-decoration: none;
+        color: white;
+    }
+    .header-title{
+        line-height: 80px;
+        padding-left: 50px;
+        font-size: 2.6rem;
+        color: white;
+    }
 </style>
