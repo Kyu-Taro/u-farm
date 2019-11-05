18afb8ef2cbf83dc4f4ace5ac32eb65541122990
Kyu Taro <kyutaro@Kyu.local>
2019-11-05 21:05:55 +0900

    メール認証修正

--- app/Http/Controllers/Admin/Auth/VerificationController.php
+++ app/Http/Controllers/Admin/Auth/VerificationController.php
@@ -51,25 +51,25 @@ class VerificationController extends Controller
 
     public function verify(Request $request)
     {
-        if($request->route('id') != $request->user()->getKey()){
+        if ($request->route('id') != $request->user()->getKey()) {
             throw new AuthorizationException;
         }
 
-        if($request->user()->markEmailAsVerified()){
+        if ($request->user()->markEmailAsVerified()) {
             event(new Verified($request->user()));
         }
 
-        return redirect($this->redirectPath())->with('admin.verified',true);
+        return redirect($this->redirectPath())->with('admin.verified', true);
     }
 
     public function resend(Request $request)
     {
-        if($request->user()->hasVerifiedEmail()){
+        if ($request->user()->hasVerifiedEmail()) {
             return redirect($this->redirectPath());
         }
 
         $request->user()->sendEmailVerificationNotification();
 
-        return back()->with('front.resent',true);
+        return back()->with('auth.verify', true);
     }
 }
