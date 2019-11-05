5f3dbe832e3c95d71455b5d313b8c3c34fd94bda
神谷友樹 <jetree900@gmail.com>
2019-11-05 23:41:03 +0900

    RegisterComponentを作成、登録

--- resources/js/app.js
+++ resources/js/app.js
@@ -23,6 +23,7 @@ window.Vue = require('vue');
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.component('header-component', require('./components/HeaderComponent.vue').default);
 Vue.component('footer-component', require('./components/FooterComponent.vue').default);
+Vue.component('registerform-component', require('./components/RegisterFormComponent.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
---
+++ resources/js/components/RegisterFormComponent.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="full-page">
+        <div class="container">
+            <ul class="form-ul">
+                <li><a href="/register">買いたい人</a></li>
+                <li ><a href="/register_second">売りたい人</a></li>
+            </ul>
+            <form action="/register" method="POST">
+                <label>名前</label>
+                <input type="text" v-model="name"><br/>
+                <label>Email</label>
+                <input type="text" v-model="email"><br/>
+                <label>パスワード</label>
+                <input type="password"  v-model="password"><br/>
+                <label>再入力</label>
+                <input type="password" v-model="password_confirmation"><br/>
+                <label>郵便番号(ハイフンなし)</label>
+                <input type="tel" v-model="postNumber"><br/>
+                <label>住所</label>
+                <input type="text" v-model="area"><br/>
+                <label>電話番号</label>
+                <input type="tel" v-model="tell"><br/>
+                <input type="submit" value="登録">
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data:{
+            name:'',
+            email:'',
+            password:'',
+            password_confirmation:'',
+            postNumber:'',
+            area:'',
+            tell:''
+        },
+        mounted() {
+            console.log('Component mounted.')
+        }
+
+    }
+</script>
+
+<style>
+    .full-page{
+        height: auto;
+        background-image: url('/img/form.jpg');
+        background-size: 100% 
+    }
+</style>
