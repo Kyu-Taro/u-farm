8adeb834e07f1dc4aeec69494758cac921797e1e
Kyu Taro <kyutaro@Kyu.local>
2019-11-06 21:21:49 +0900

    メール修正

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -14,7 +14,7 @@ class MainController extends Controller
     public function index()
     {
         return view('pages.index');
-        // return view('index');
+        // return view('index');//
     }
 
     public function register_second()
