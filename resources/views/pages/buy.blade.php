fc57632ebda3dd340296ec3eabc74ed2b3c37171
Kyu Taro <kyutaro@Kyu.local>
2019-11-09 18:42:39 +0900

    個数入力

--- resources/views/pages/buy.blade.php
+++ resources/views/pages/buy.blade.php
@@ -1,3 +1,3 @@
-【購入商品】{{ session('name') }}<br/>
-【個数】{{ session('number') }}<br/>
-【合計金額】{{ session('price') }}<br/>
+【購入商品】{{ session($data['name']) }}<br/>
+【個数】{{ session($data['number']) }}<br/>
+【合計金額】{{ session($data['price']) }}<br/>
