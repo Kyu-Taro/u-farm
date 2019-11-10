e508bf792a60304b7518dcd59cb7a2af790aac72
Kyu Taro <kyutaro@Kyu.local>
2019-11-10 20:26:59 +0900

    commit

--- routes/web.php
+++ routes/web.php
@@ -32,7 +32,7 @@ Route::group(['middleware' => 'auth:user'],function(){
     Route::resource('/user','UserController');
     Route::get('/user_delete','MainController@user_delete');
     Route::post('/charge','MainController@charge');
-    Route::post('/buy','MainController@buy')->name('buy');
+    Route::get('/buy','MainController@buy')->name('buy');
 });
 
 // Route::group(['middleware' => 'auth:admin','middleware' => 'verified'],function(){
