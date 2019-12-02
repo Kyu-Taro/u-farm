3fcadfdb020e7879c8bf5cf2e3a502b814dc770a
Kyu Taro <kyutaro@Kyu.local>
2019-12-02 20:59:23 +0900

    ルートの作成と不要ファイルの削除

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,9 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
+//適当なアドレスにアクセスあった際には必ずこのページにアクセスさせる
+Route::get('/{any?}',function(){
     return view('welcome');
-});
+})->where('any','.+');
 
+//Authのルート
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
