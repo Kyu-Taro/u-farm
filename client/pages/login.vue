3687cb28a41afaeca94c3d0d5e4618a8b9440247
神谷友樹 <jetree900@gmail.com>
2019-12-06 22:08:37 +0900

    loginFormを実装

--- client/pages/login.vue
+++ client/pages/login.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="container">
-    main
+    <form @submit.prevent="login" action="/login" method="POST">
+      <input type="hidden">
+      <div class="form-group">
+        <label>メールアドレス</label>
+        <input v-model="loginForm.email" name="email" type="text">
+      </div>
+      <div class="form-group">
+        <label>パスワード</label>
+        <input v-model="loginForm.password" name="password" type="password" autocomplete="off">
+      </div>
+      <input type="submit" value="ログイン">
+    </form>
   </div>
 </template>
 
@@ -13,6 +24,19 @@ export default {
   head () {
     return {
       title: 'login'
+    }
+  },
+  data: () => {
+    return {
+      loginForm: {
+        email: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    login () {
+      console.log(this.loginForm)
     }
   }
 }
