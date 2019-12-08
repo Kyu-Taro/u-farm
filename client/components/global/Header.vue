030e9c373bfce5e47ab08796f3892b82aed85412
神谷友樹 <jetree900@gmail.com>
2019-12-08 20:31:07 +0900

    headerにlinkを

--- client/components/global/Header.vue
+++ client/components/global/Header.vue
@@ -8,7 +8,9 @@
               fresh vesitables fo you
             </div>
             <div class="col-xs-12">
-              U_FARM
+              <Router-Link to="/">
+                U_FARM
+              </Router-Link>
             </div>
           </div>
         </div>
@@ -16,16 +18,25 @@
       <div class="col-xs-6 end-xs">
         <div class="row middle-xs">
           <div class="col-sm-3 hidden-xs">
-            ホーム
+            <Router-Link to="/">
+              ホーム
+            </Router-Link>
           </div>
           <div class="col-sm-3 hidden-xs">
-            農家登録はこちら
+            <!-- 仮です -->
+            <Router-Link to="/register">
+              農家登録はこちら
+            </Router-Link>
           </div>
           <div class="col-xs-6 col-sm-3">
-            新規会員登録
+            <Router-Link to="/register">
+              新規会員登録
+            </Router-Link>
           </div>
           <div class="col-xs-6 col-sm-3">
-            ログイン
+            <Router-Link to="/login">
+              ログイン
+            </Router-Link>
           </div>
         </div>
       </div>
