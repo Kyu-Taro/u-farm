bb89b41707a202bfe70878b6359671f64d2c0ebd
K.Saito <>
2019-12-09 23:10:32 +0900

    農家ページを作りました。サイドナビは未実装

---
+++ client/components/global/ItemCard.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <div>
+      <img :src="itemImage">
+      <div v-if="canEdit">
+        edit
+      </div>
+      <div>
+        {{ price }}円
+      </div>
+    </div>
+    <div>
+      <div>
+        <span>
+          【{{ productionArea }}】
+        </span>
+        <span>
+          産地直送
+        </span>
+      </div>
+      <div>
+        {{ itemName }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    itemName: {
+      type: String,
+      required: true
+    },
+    price: {
+      type: Number,
+      required: true
+    },
+    productionArea: {
+      type: String,
+      required: true
+    },
+    itemImage: {
+      type: String,
+      required: true
+    },
+    canEdit: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ client/pages/users/_id/farmer.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="container">
+    <div>
+      <ItemCard
+        v-for="(item, idx) in items"
+        :key="idx"
+        v-bind="item"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import ItemCard from '~/components/global/ItemCard'
+
+const DUMMY_ITEM = {
+  itemName: 'itemName',
+  price: 1000,
+  productionArea: '東京',
+  itemImage: 'https://4.imimg.com/data4/UB/WV/MY-32247465/fresh-red-apple-500x500.jpg'
+}
+
+export default {
+  layout: 'default',
+  components: {
+    ItemCard
+  },
+  validate ({ params }) {
+    return true
+  },
+  data () {
+    return {
+      items: [DUMMY_ITEM]
+    }
+  },
+  computed: {
+    userName () {
+      return this.$route.params.id
+    }
+  },
+  mounted () {
+    this.fetchItems({})
+  },
+  methods: {
+    fetchItems (query) {
+      // サーバーから取得
+      this.items = this.items.concat(
+        Array.from(new Array(10)).map((v, i) => DUMMY_ITEM)
+      )
+      return true
+    }
+  },
+  head () {
+    return {
+      title: this.userName
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
