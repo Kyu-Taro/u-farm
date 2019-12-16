16e7b3ea7742633c5cdbe65a587368f4dacbd98a
K.Saito <>
2019-12-16 23:16:30 +0900

    ログイン中に登録しようとした場合エラーを返す

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Response;
 
 class RedirectIfAuthenticated
 {
@@ -18,7 +19,13 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect('/home');
+            return response()->json([
+                'meta'=> [
+                    'status'=> 'error'
+                ]
+            ],
+            400
+        );
         }
 
         return $next($request);
