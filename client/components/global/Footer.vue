9a9d1fdb3807714f45e2ed838e3484765156fb7b
hikaru <hikaruchanneru3@gmail.com>
2019-12-23 21:27:11 +0900

    footer_coding

--- client/components/global/Footer.vue
+++ client/components/global/Footer.vue
@@ -1,24 +1,24 @@
 <template>
   <footer>
-    <div class="row center-xs">
-      <div class="col-xs-12">
-        <ul>
-          <li>
-            link
+    <div class="row center-xs footer">
+      <div class="col-xs-12 footer__nav">
+        <ul class="row center-xs">
+          <li class="footer__nav-link">
+            ホーム
           </li>
-          <li>
-            link
+          <li class="footer__nav-link">
+            ログイン
           </li>
-          <li>
-            link
+          <li class="footer__nav-link">
+            新規登録
           </li>
-          <li>
-            link
+          <li class="footer__nav-link">
+            商品一覧
           </li>
         </ul>
       </div>
-      <div class="col-xs-12">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi, cum?
+      <div class="col-xs-12 footer__text">
+        Copyright ©︎ 2019 U-Farm All rights reserved.
       </div>
     </div>
   </footer>
@@ -30,4 +30,18 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.footer {
+  background: #D5D5D5;
+  padding: 30px 0;
+  &__nav {
+    padding: 30px 0;
+    &-link {
+      padding: 0 10px;
+      border-right: 1px solid #33302F;
+      &:last-child {
+        border-right: none;
+      }
+    }
+  }
+}
 </style>
