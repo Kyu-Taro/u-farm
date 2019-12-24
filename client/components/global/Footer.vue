67de8bb4e9523bbd8a638b43ac6457ddcb8fa039
hikaru <hikaruchanneru3@gmail.com>
2019-12-24 21:12:28 +0900

    Leeさんのヘッダー修正も合わせてコミット

--- client/components/global/Footer.vue
+++ client/components/global/Footer.vue
@@ -1,19 +1,30 @@
 <template>
-  <footer>
-    <div class="row center-xs footer">
+  <footer class="footer container">
+    <div class="row center-xs">
       <div class="col-xs-12 footer__nav">
         <ul class="row center-xs">
           <li class="footer__nav-link">
-            ホーム
+            <nuxt-Link to="/">
+              ホーム
+            </nuxt-Link>
+          </li>
+          <li v-if="isLogin" @click="logout" class="footer__nav-link">
+            ログアウト
+          </li>
+          <li v-else class="footer__nav-link">
+            <nuxt-Link to="login">
+              ログイン
+            </nuxt-Link>
           </li>
           <li class="footer__nav-link">
-            ログイン
+            <nuxt-Link to="/register">
+              新規登録
+            </nuxt-Link>
           </li>
           <li class="footer__nav-link">
-            新規登録
-          </li>
-          <li class="footer__nav-link">
-            商品一覧
+            <nuxt-Link to="/items">
+              商品一覧
+            </nuxt-Link>
           </li>
         </ul>
       </div>
@@ -26,18 +37,34 @@
 
 <script>
 export default {
+  computed: {
+    isLogin () {
+      return this.$store.getters['auth/check']
+    }
+  },
+  methods: {
+    async logout () {
+      await this.$store.dispatch('auth/getCsrfToken')
+      await this.$store.dispatch('auth/logout')
+      this.$router.push('/')
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 .footer {
+  width: 100%;
+  padding-top: 30px;
+  padding-bottom: 30px;
   background: #D5D5D5;
-  padding: 30px 0;
   &__nav {
-    padding: 30px 0;
+    padding-top: 30px;
+    padding-bottom: 30px;
     &-link {
-      padding: 0 10px;
+      padding: 0 15px;
       border-right: 1px solid #33302F;
+      cursor: pointer;
       &:last-child {
         border-right: none;
       }
--- client/components/global/Header.vue
+++ client/components/global/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="header">
+  <header class="header container">
     <div>
       <h1>
         ログイン確認用
@@ -38,7 +38,19 @@
         <div class="row">
           <div class="col-sm-3 hidden-xs">
             <div class="row middle-xs nav__item nav__item--home">
-              <nuxt-link to="/" class="nav-item">
+              <nuxt-link v-if="isLogin && farmer_flg" to="/" class="nav-item">
+                <fa class="nav-item__icon" icon="home" />
+                <div class="nav-item__text">
+                  ホーム
+                </div>
+              </nuxt-link>
+              <nuxt-link v-else-if="isLogin" to="/" class="nav-item">
+                <fa class="nav-item__icon" icon="home" />
+                <div class="nav-item__text">
+                  ホーム
+                </div>
+              </nuxt-link>
+              <nuxt-link v-else to="/" class="nav-item">
                 <fa class="nav-item__icon" icon="home" />
                 <div class="nav-item__text">
                   ホーム
@@ -46,7 +58,7 @@
               </nuxt-link>
             </div>
           </div>
-          <div class="col-sm-3 hidden-xs">
+          <div v-if="!isLogin" class="col-sm-3 hidden-xs">
             <div class="row middle-xs nav__item nav__item--farmer">
               <nuxt-Link to="/registerAsFarmer" class="nav-item">
                 <fa class="nav-item__icon" icon="user" />
@@ -56,7 +68,7 @@
               </nuxt-Link>
             </div>
           </div>
-          <div class="col-xs-6 col-sm-3">
+          <div v-if="!isLogin" class="col-xs-6 col-sm-3">
             <div class="row middle-xs nav__item nav__item--register">
               <nuxt-Link to="/register" class="nav-item">
                 <fa class="nav-item__icon" icon="pen" />
@@ -133,10 +145,6 @@ header {
     &__item {
       height: $nav-height;
       padding: 10px;
-      a:link {
-        color: inherit;
-        text-decoration: none;
-      }
       &--register {
         color: #fff;
         background: #639E56;
@@ -156,11 +164,9 @@ header {
       &__icon {
         font-size: $nav-item-icon-size;
         margin-right: $nav-item-icon-margin;
-        color: inherit;
       }
       &__text {
         flex: 1;
-        color: inherit;
       }
     }
   }
