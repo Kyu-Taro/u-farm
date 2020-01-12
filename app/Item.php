b3668a474cba0055fb1c3a22e92fab60de3494db
神谷友樹 <jetree900@gmail.com>
2020-01-13 00:08:27 +0900

    itemモデルに入力可のものを指定

--- app/Item.php
+++ app/Item.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Item extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'farmer_id', 'img', 'name','price','sipping_duration','text','recipe','area','detail_img','detail_prof'
+    ];
 }
